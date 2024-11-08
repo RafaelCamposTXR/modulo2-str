b4219c4ada367ab5ed5d44ba84b895c838009461
Eymi Montúfar <43094416+EymiM@users.noreply.github.com>
2024-11-08 20:23:08 -0300

    Atualizei IED.cs considerando ele como uma thread

--- Modulo2STR/Core/Models/IED.cs
+++ Modulo2STR/Core/Models/IED.cs
@@ -1,14 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Modulo2STR.Core.Models
+public class IED
 {
-    internal class IED
+    public string Id { get; private set; }
+    private float corrente;
+    private readonly Protecao50 protecao50;
+    private readonly Protecao51 protecao51;
+
+    public IED(string id)
     {
+        Id = id;
+        protecao50 = new Protecao50(id);
+        protecao51 = new Protecao51(id);
+    }
 
+    public float Corrente
+    {
+        get { return corrente; }
+        set
+        {
+            if (corrente != value)
+            {
+                corrente = value;
+                VerificarProtecoes();
+            }
+        }
+    }
 
+    // Método para iniciar o monitoramento em uma thread
+    public void IniciarMonitoramento()
+    {
+        new Thread(() =>
+        {
+            while (true)
+            {
+                // Monitoramento contínuo com um intervalo de tempo
+                VerificarProtecoes();
+                Thread.Sleep(1000); // Intervalo de verificação
+            }
+        }).Start();
+    }
+
+    private void VerificarProtecoes()
+    {
+        Console.WriteLine($"IED {Id}: Verificando proteções para corrente = {corrente} A");
+
+        // Verifica proteção 50
+        if (protecao50.VerificarSobrecorrente(corrente))
+        {
+            protecao50.EmitirAlerta();
+        }
+
+        // Verifica proteção 51
+        if (protecao51.VerificarSobrecorrente(corrente))
+        {
+            protecao51.EmitirAlerta();
+        }
     }
 }
