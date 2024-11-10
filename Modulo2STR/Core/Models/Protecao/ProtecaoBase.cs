ab838e57686cae327378012dcf7dcee5a5a73ceb
Eymi Montúfar <43094416+EymiM@users.noreply.github.com>
2024-11-10 04:42:11 -0300

    Atualizando ProtecaoBase.cs

--- Modulo2STR/Core/Models/Protecao/ProtecaoBase.cs
+++ Modulo2STR/Core/Models/Protecao/ProtecaoBase.cs
@@ -1,25 +1,30 @@
-﻿using System;
+using System;
 
 public abstract class ProtecaoBase
 {
     public string? IED { get; set; }
-    public float? limiar { get; set; }
+    public float? limiar50 { get; set; }
 
-    public ProtecaoBase(string? ied, float? limiar)
+    public float? limiar51 { get; set; }
+
+    //private readonly GerenciaAlerta gerenciaAlerta;
+
+    public ProtecaoBase(string? ied, float? limiar50, float? limiar51)
     {
         this.IED = ied;
-        this.limiar = limiar;
+        this.limiar50 = limiar50;
+        this.limiar51 = limiar51;
     }
 
 
     public abstract bool verificarSobrecorrente(float correnteAtual);
 
 
-    public String EmitirAlerta()
+    public void EmitirAlerta()
     {
-        return $"Alerta! Sobrecorrente detectada no IED: {IED}. Limite de {limiar}A ultrapassado.";
+        // aqui era pra ser o começo de gerar pacote broadcast:
+        //var mensagem = new MensagemBroadcast(IED, limiar.Value, "Sobrecorrente Instantânea");
+        //gerenciaAlerta.EnviarAlerta(mensagem);
+        Console.WriteLine($"\nAlerta! Sobrecorrente detectada no IED: {IED}.");
     }
 }
-
-
-
