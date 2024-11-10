af89521c4107ba1b6ed947c35c0ce41c8eae0757
Eymi Montúfar <43094416+EymiM@users.noreply.github.com>
2024-11-10 04:41:26 -0300

    Atualizando Protecao50.cs

--- Modulo2STR/Core/Models/Protecao/Protecao50.cs
+++ Modulo2STR/Core/Models/Protecao/Protecao50.cs
@@ -1,12 +1,20 @@
-﻿public class Protecao50 : ProtecaoBase
+public class Protecao50 : ProtecaoBase
 {
-    public Protecao50(string? IED = null, float? limiar = null)
-        : base(IED, limiar ?? 100f)  
+    public Protecao50(string? IED = null, float? limiar50 = null, float? limiar51 = null)
+        : base(IED, limiar50 ?? 500f, limiar51 ?? 100f)  
     {
     }
 
     public override bool verificarSobrecorrente(float corrente)
     {
-        return corrente > limiar;
+        if (corrente > limiar50)
+        {
+            EmitirAlerta();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
